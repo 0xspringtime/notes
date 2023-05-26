9decc92f0117f7d2fb424e4815d026a383c1d209
0xspringtime <0xspringtime@protonmail.com>
2023-05-26 19:28:29 -0400

    commit

---
+++ transformers/02/n.md
@@ -0,0 +1,4 @@
+- Transformer models have a maximum input sequence length that is referred to as the
+maximum context size
+
+
---
+++ transformers/02/test.py
@@ -0,0 +1,14 @@
+from datasets import list_datasets
+all_datasets = list_datasets()
+print(f"There are {len(all_datasets)} datasets currently available on the Hub")
+print(f"The first 10 are: {all_datasets[:10]}")
+from datasets import load_dataset
+emotions = load_dataset("emotion")
+import pandas as pd
+emotions.set_format(type="pandas")
+df = emotions["train"][:]
+df.head()
+
+import torch
+import torch.nn.functional as F
+input_ids = torch.tensor(input_ids)
